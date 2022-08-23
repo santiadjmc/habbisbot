333ffd6ba9f258dbb8d9ce6f7ae123068c7c08e7
santiadjmc <santiadjmc@gmail.com>
2022-08-23 17:08:06 -0400

    Added semicolon

--- mysql/queries.ts
+++ mysql/queries.ts
@@ -1,5 +1,5 @@
 import Log from "../Log";
 import db from "./database";
 export default function queries(): void {
-    Log.info("database", "There are no queries to execute.")
+    Log.info("database", "There are no queries to execute.");
 };
