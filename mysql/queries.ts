cb3b824a8fb40c3ac7ce863e6dcf384a4cbfb01f
santiadjmc <santiadjmc@gmail.com>
2022-08-23 17:13:59 -0400

    Added table

--- mysql/queries.ts
+++ mysql/queries.ts
@@ -1,5 +1,5 @@
 import Log from "../Log";
 import db from "./database";
 export default function queries(): void {
-    Log.info("database", "There are no queries to execute.");
+    db.query("CREATE TABLE IF NOT EXISTS discord_warnings (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, userid VARCHAR(255) NOT NULL, reason VARCHAR(255) NOT NULL DEFAULT 'no reason', authorid VARCHAR(255) NOT NULL, createAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)");
 };
