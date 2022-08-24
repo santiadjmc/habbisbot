0645b0d1a78a4174ef0386e3f9b75ea4261e0715
santiadjmc <santiadjmc@gmail.com>
2022-08-23 20:09:42 -0400

    Updated discord_warnings table

--- mysql/queries.ts
+++ mysql/queries.ts
@@ -1,5 +1,5 @@
 import Log from "../Log";
 import db from "./database";
 export default function queries(): void {
-    db.query("CREATE TABLE IF NOT EXISTS discord_warnings (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, userid VARCHAR(255) NOT NULL, reason VARCHAR(255) NOT NULL DEFAULT 'no reason', authorid VARCHAR(255) NOT NULL, createAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)");
+    db.query("CREATE TABLE IF NOT EXISTS discord_warnings (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, userid VARCHAR(255) NOT NULL, reason VARCHAR(255) NOT NULL DEFAULT 'no reason', authorid VARCHAR(255) NOT NULL, createdAt INT(255) NOT NULL)");
 };
