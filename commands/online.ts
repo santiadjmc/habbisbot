206d9542ea8635b2602622341c2ca3b9e0364b09
santiadjmc <santiadjmc@gmail.com>
2022-08-23 20:32:52 -0400

    Created online command

---
+++ commands/online.ts
@@ -0,0 +1,13 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
+import db from "../mysql/database";
+export default {
+    data: new SlashCommandBuilder()
+    .setName("online")
+    .setDescription("Muestra una lista de usuarios conectados al hotel"),
+    execute: async (interaction: ChatInputCommandInteraction) => {
+        await interaction.deferReply();
+        const users = ((await db.query("SELECT * FROM users") as unknown) as any[]);
+        const onlineUsers = users.filter(u => String(u.online) !== "0");
+        await interaction.editReply("```\n"+ `[${onlineUsers.length}] Usuarios conectados:\n` + onlineUsers.map(u => String(u.online) === "1" ? u.username : `${u.username} (Ausente)`).join("\n") + "\n```");
+    }
+}
